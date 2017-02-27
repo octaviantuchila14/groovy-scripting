7b6f058981497794016b3df50fd985892b03985f
octaviantuchila14 <octavian.tuchila12@imperial.ac.uk>
2017-02-27 18:14:51 +0000

    Added stack trace

--- src/main.groovy
+++ src/main.groovy
@@ -15,3 +15,15 @@ def jsonData = jsonSlurper.parseText(data)
 def level = jsonData.level
 
 assert level == "OK"
+
+Exception is: hudson.AbortException: Couldn't find any revision to build. Verify the repository and branch configuration for this job.
+
+
+Stack trace: [hudson.plugins.git.GitSCM.determineRevisionToBuild(GitSCM.java:1002), hudson.plugins.git.GitSCM.checkout(GitSCM.java:1098), org.jenkinsci.plugins.workflow.steps.scm.SCMStep.checkout(SCMStep.java:109), org.jenkinsci.plugins.workflow.steps.scm.SCMStep$StepExecutionImpl.run(SCMStep.java:83), org.jenkinsci.plugins.workflow.steps.scm.SCMStep$StepExecutionImpl.run(SCMStep.java:73), org.jenkinsci.plugins.workflow.steps.AbstractSynchronousNonBlockingStepExecution$1$1.call(AbstractSynchronousNonBlockingStepExecution.java:47), hudson.security.ACL.impersonate(ACL.java:260), org.jenkinsci.plugins.workflow.steps.AbstractSynchronousNonBlockingStepExecution$1.run(AbstractSynchronousNonBlockingStepExecution.java:44), java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:511), java.util.concurrent.FutureTask.run(FutureTask.java:266), java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1142), java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:617), java.lang.Thread.run(Thread.java:745)]
+
+checkout([$class: 'GitSCM',
+        branches: [[name: "origin/pr/${pullRequestId}/merge"]], doGenerateSubmoduleConfigurations: false,
+        extensions: [], gitTool: 'jgit', submoduleCfg: [],
+        userRemoteConfigs: [[credentialsId: credentials,
+                             name: 'origin', refspec: '+refs/pull-requests/*:refs/remotes/origin/pr/*',
+                             url: repository]]])
